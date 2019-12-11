aebfc7a8a958984d9a0aa5459cf0c11a1f14355b
RGMB3 <Russellmarbut@yahoo.com>
2019-12-10 22:23:06 -0600

    Inital changes .2

--- src/components/header.js
+++ src/components/header.js
@@ -5,7 +5,7 @@ import React from "react"
 const Header = ({ siteTitle }) => (
   <header
     style={{
-      background: `rebeccapurple`,
+      background: `#659DBD`,
       marginBottom: `1.45rem`,
     }}
   >
